00a9937f39df6a44853450d87153087f1ac15486
Alexey Pyltsyn <lex61rus@gmail.com>
2019-10-03 23:05:19 +0300

    Safe replace insecure HTTP links with HTTPS

--- src/data/logos.js
+++ src/data/logos.js
@@ -12,7 +12,7 @@ export const openSourceData = [
   {
     name: 'Digital State',
     logo: 'digital-state',
-    link: 'http://www.digitalstate.ca/',
+    link: 'https://www.digitalstate.ca/',
   },
   {
     name: 'AudienceHero',
@@ -45,12 +45,12 @@ export const otherData = [
   {
     name: 'Alstom',
     logo: 'alstom',
-    link: 'http://www.alstom.com/',
+    link: 'https://www.alstom.com/',
   },
   {
     name: 'beIN SPORTS',
     logo: 'bein-sports',
-    link: 'http://www.beinsports.com/',
+    link: 'https://www.beinsports.com/',
   },
   {
     name: 'Biig',
@@ -85,7 +85,7 @@ export const otherData = [
   {
     name: 'Exaqtworld',
     logo: 'exaqtworld',
-    link: 'http://www.exaqtworld.com/',
+    link: 'https://www.exaqtworld.com/',
   },
   {
     name: 'The Fork',
@@ -95,7 +95,7 @@ export const otherData = [
   {
     name: 'France 24',
     logo: 'france-24',
-    link: 'http://www.france24.com/',
+    link: 'https://www.france24.com/',
   },
   {
     name: 'General Electric',
@@ -130,12 +130,12 @@ export const otherData = [
   {
     name: 'M6',
     logo: 'm6',
-    link: 'http://www.groupem6.fr/',
+    link: 'https://www.groupem6.fr/',
   },
   {
     name: 'MEL',
     logo: 'mel',
-    link: 'http://www.lillemetropole.fr/',
+    link: 'https://www.lillemetropole.fr/',
   },
   {
     name: 'Microdon',
@@ -170,7 +170,7 @@ export const otherData = [
   {
     name: 'Ville de Roubaix',
     logo: 'roubaix',
-    link: 'http://www.ville-roubaix.fr/',
+    link: 'https://www.ville-roubaix.fr/',
   },
   {
     name: 'Saveur Bière',
@@ -195,7 +195,7 @@ export const otherData = [
   {
     name: 'TMM',
     logo: 'tmm',
-    link: 'http://www.tmm-groupe.com/',
+    link: 'https://www.tmm-groupe.com/',
   },
   {
     name: 'whatwedo',
